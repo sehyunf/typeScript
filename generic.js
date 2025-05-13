8515733648dba948e08ad910524fb5f0e3ac5530
sehyunf <ssam010925@gmail.com>
2025-05-13 12:06:40 +0900

    generic2

--- generic.js
+++ generic.js
@@ -2,6 +2,16 @@
 // <T>() : 어떤 타입을 받을건지
 // (params:T) : 들어온 param의 타입
 // (): T : return 타입
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // 함수의 선언
 function genericText(value) {
     console.log(value);
@@ -12,8 +22,8 @@ const genericText2 = (param) => {
     return param;
 };
 // 함수의 사용
-genericText("a");
-genericText(true);
+// genericText<string>("a")
+// genericText<boolean>(true)
 // 제네릭 문법의 타입가드
 const genericText3 = (value) => {
     if (typeof value === "string") {
@@ -25,3 +35,29 @@ const myFunc2 = (text) => {
     return text;
 };
 let myString = myFunc2;
+// const getProductItems = <T extends keyof ProductItem>(item: T): T => {
+//   return item
+// }
+const getProductItems = (item) => {
+    return item;
+};
+const item = {
+    name: "상품",
+    price: 20000,
+    stock: 20,
+};
+getProductItems(item);
+const getUserName = (data) => {
+    console.log(data.name);
+};
+const fetching = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("https://jsonplaceholder.typicode.com/users");
+    const datas = yield response.json();
+    return datas;
+});
+fetching()
+    .then((datas) => {
+    datas.forEach((data) => {
+        getUserName(data);
+    });
+});
--- generic.ts
+++ generic.ts
@@ -14,8 +14,8 @@ const genericText2 = <T>(param: T): T => {
 }
 
 // 함수의 사용
-genericText<string>("a")
-genericText<boolean>(true)
+// genericText<string>("a")
+// genericText<boolean>(true)
 
 // 제네릭 문법의 타입가드
 const genericText3 = <T>(value: T): T | Number => {
@@ -36,3 +36,114 @@ const myFunc2 = <T>(text: T): T => {
 }
 
 let myString:GenericLogTextFunc = myFunc2;
+
+// 제네릭 문법 키 제한
+interface ProductItem {
+  name: string;
+  price : number;
+  stock : number;
+}
+
+// const getProductItems = <T extends keyof ProductItem>(item: T): T => {
+//   return item
+// }
+
+const getProductItems = <T extends ProductItem>(item: T): T => {
+  return item
+}
+
+const item: ProductItem = {
+  name : "상품",
+  price : 20000,
+  stock : 20,
+}
+
+getProductItems<ProductItem>(item);
+// getProductItems({name : "컴퓨터", price: 10000, stock: 20, address : ""})
+
+// 제네릭 문법을 이용한 REST API 사용 실습
+// https://jsonplaceholder.typicode.com/posts
+// 1) fetching 후 전체를 console.log로 출력
+// 2) fetching 후 (unknown 오류 생김)
+
+// ================================실습1================================
+// interface UserData {
+//   userId: number;
+//   id : number;
+//   title : string;
+//   body : string
+// }
+
+// const getUserData = <T extends UserData>(data: T) => {
+//   console.log(data)
+// }
+
+// const fetching = async <UserData>(): Promise<UserData[]> => {
+//   const response: Response = await fetch("https://jsonplaceholder.typicode.com/posts")
+//   const datas: UserData[] = await response.json();
+
+//   return datas;
+// }
+
+// // fetching<UserData>()
+// //   .then((datas: UserData[]) => {
+// //     datas.forEach((data: UserData) => {
+// //       getUserData<UserData>(data)    
+// //     });
+// // })
+
+// fetching<UserData>()
+//   .then((datas: UserData[]) => {
+//     datas.forEach(getUserData)
+// })
+
+// ================================실습2================================
+export interface UserData {
+    id:       number;
+    name:     string;
+    username: string;
+    email:    string;
+    address:  Address;
+    phone:    string;
+    website:  string;
+    company:  Company;
+}
+
+export interface Address {
+    street:  string;
+    suite:   string;
+    city:    string;
+    zipcode: string;
+    geo:     Geo;
+}
+
+export interface Geo {
+    lat: string;
+    lng: string;
+}
+
+export interface Company {
+    name:        string;
+    catchPhrase: string;
+    bs:          string;
+}
+
+const getUserName = <T extends UserData>(data: T) => {
+  console.log(data.name)
+}
+
+const fetching = async <UserData>(): Promise<UserData[]> => {
+  const response: Response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const datas: UserData[] = await response.json();
+
+  return datas;
+}
+
+fetching<UserData>()
+  .then((datas: UserData[]) => {
+    datas.forEach((data: UserData) => {
+      getUserName<UserData>(data)
+    });
+})
+
+
